b6cb5a749ad8c5e056067c70cb85fd3fc0669f8e
Isabellaholowka <163411298+Isabellaholowka@users.noreply.github.com>
2024-10-17 20:04:11 -0300

    adicionei um código em python

---
+++ atividade.py
@@ -0,0 +1,4 @@
+print ("Para realizar o seu cadastro, por favor preencha os campos abaixo:")
+Nome= input ("Digite seu nome completo: ")
+Email= input ("Digite seu E-mail: ")
+Cpf= input ("Digite seu Cpf: ") 
